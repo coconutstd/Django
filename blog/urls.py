9e8c1969cfaf1a8fc4972dad33224aab406609d2
coconut <wnsspdl@naver.com>
2019-05-09 16:27:29 +0900

    post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import path, include
 from . import views
 urlpatterns = [
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
