02e431d73224a51f537131a209ccfd8d7b20da61
coconut <wnsspdl@naver.com>
2019-05-23 13:27:51 +0900

    tag model tested

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,14 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+
 
 
 
@@ -30,6 +38,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=True)
 
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
