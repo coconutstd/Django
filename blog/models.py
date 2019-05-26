879deb97087ed7ce22e254d7a90d9fbcc7c73bbd
coconut <wnsspdl@naver.com>
2019-05-26 17:03:56 +0900

    markdown on post; tags can be blank

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 
 class Category(models.Model):
     name = models.CharField(max_length=25, unique=True)
@@ -30,7 +31,7 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField()
+    content = MarkdownxField()
 
     head_Image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
 
@@ -38,10 +39,14 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=True)
 
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
 
     def get_absolute_url(self):
         return '/blog/{}/'.format(self.pk)
+
+    def get_markdown_content(self):
+        return markdown(self.content)
+
--- my_project/settings.py
+++ my_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'markdownx',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 MEDIA_URL = '/media/'
+
+from datetime import datetime
+
+MARKDOWNX_MEDIA_PATH = datetime.now().strftime('markdownx/%Y/%m/%d')
